44b412c33b1d4b36ef83609ee5e069cc8a9ff9a9
antonydanon <antondanik32@mail.ru>
2023-07-05 21:02:31 +0300

    bug(BF-211): add condition for check count of books in the storage

--- src/main/java/by/bookify/book/exception/BookExceptionHandler.java
+++ src/main/java/by/bookify/book/exception/BookExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
@@ -13,5 +14,10 @@ public class BookExceptionHandler {
     @ExceptionHandler(BookNotFoundException.class)
     public ResponseEntity<ExceptionResponse> handleBookNotFound(BookNotFoundException e) {
         return ResponseEntity.status(NOT_FOUND).body(new ExceptionResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(NotEnoughBooksInStorageException.class)
+    public ResponseEntity<ExceptionResponse> handleNotEnoughBooksInStorage(NotEnoughBooksInStorageException e) {
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ExceptionResponse(e.getMessage()));
     }
 }
